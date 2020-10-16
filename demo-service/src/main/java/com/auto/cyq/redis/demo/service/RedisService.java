b0c286d58057cf58bb357d7a8758c86a83109ae7
wangbisheng <wangbisheng@autohome.com.cn>
2020-10-16 10:35:44 +0800

    init cluster

---
+++ demo-service/src/main/java/com/auto/cyq/redis/demo/service/RedisService.java
@@ -0,0 +1,34 @@
+package com.auto.cyq.redis.demo.service;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.stereotype.Service;
+
+import java.util.concurrent.TimeUnit;
+
+@Slf4j
+@Service
+public class RedisService {
+    @Autowired
+    StringRedisTemplate stringRedisTemplate;
+
+    public void block(String key) {
+        try {
+            stringRedisTemplate.opsForList().leftPop(key, 5, TimeUnit.MINUTES);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    public void setNoBlock(String key, String value) {
+        try {
+            stringRedisTemplate.opsForValue().set(key, value, 5, TimeUnit.MINUTES);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+    }
+
+}
---
+++ demo-web-api/src/main/java/com/auto/cyq/redis/demo/controller/DemoCtroller.java
@@ -0,0 +1,45 @@
+package com.auto.cyq.redis.demo.controller;
+
+import com.auto.cyq.redis.demo.service.RedisService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author wbs
+ * @date 2020/9/2
+ */
+@RestController
+public class DemoCtroller {
+
+    @Autowired
+    private RedisService redisService;
+
+    @GetMapping("testBlock")
+    public String setNoBlock() {
+        for (int i = 0; i < 3; i++) {
+            Thread thread = new Thread(() -> {
+                redisService.block("k");
+            });
+            thread.setName("setValueToRedis-" + (i + 1));
+            thread.start();
+
+        }
+
+        return "ok";
+    }
+
+    @GetMapping("testNoBlock")
+    public String blockList() {
+        for (int i = 0; i < 1; i++) {
+            Thread thread = new Thread(() -> {
+                redisService.setNoBlock("k", "v");
+            });
+            thread.setName("setValueToRedis-" + (i + 1));
+            thread.start();
+        }
+
+        return "ok";
+    }
+
+}
